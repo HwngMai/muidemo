4a3306cd4a890fa9b086a1132fe2d721c3621c3b
Hung Mai <hwngmvi@gmail.com>
2022-10-31 16:29:02 +0700

    commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Container, Typography, Paper, Grid } from "@mui/material";
 function Home() {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3001/customer");
+      const responseJSON = await response.json();
+      setCustomers(responseJSON);
+    };
+    fetchData();
+  }, []);
   return (
     <Container>
       <Typography variant='h3' gutterBottom align='center'>
         {" "}
         APP TO MANAGE CUSTOMERS
       </Typography>
+      <Grid container p={5} spacing={5}>
+        {customers &&
+          customers.map((customer) => (
+            <Grid xs={4} item key={customer.id}>
+              <Paper>
+                {" "}
+                <Typography align='center' variant='h5' gutterBottom>
+                  {" "}
+                  {customer.name}
+                </Typography>
+                <Typography align='center' variant='h5'>
+                  {" "}
+                  {customer.detail}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+      </Grid>
     </Container>
   );
 }
